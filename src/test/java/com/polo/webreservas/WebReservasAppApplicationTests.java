add590c25fc0fa0cdd782914c2b80d25294d41cb
Deivi De la Vega <deividelavegab@gmail.com>
2025-06-06 20:06:44 -0500

    Start proyect :rocket:

---
+++ src/test/java/com/polo/webreservas/WebReservasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polo.webreservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebReservasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
